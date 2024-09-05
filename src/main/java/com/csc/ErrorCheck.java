6787bfe1b959694cdc2a90af7237c68e1d0e7b41
TeejCam <tjcamalan@gmail.com>
2024-09-04 21:43:10 -0500

    Added finishing touches.

--- src/main/java/com/csc/ErrorCheck.java
+++ src/main/java/com/csc/ErrorCheck.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 // Your code goes here!
 public class ErrorCheck
 {
-    public static int getValue (String welcPrompt)
+    public static int getValue (String welcPrompt, int lowBound, int upBound, String error, String exPrompt)
     {
         int userInt;
         System.out.println(welcPrompt);
         Scanner scanner = new Scanner(System.in);
         userInt = scanner.nextInt();
 
-        while (userInt < -10 || userInt > 10) {
-        System.out.println("Your number is not within my chosen bounds. Enter again: ");
-        userInt = scanner.nextInt();
+        while (userInt < lowBound || userInt > upBound) {
+            System.out.println(error);
+            userInt = scanner.nextInt();
        }
-        System.out.println("Goodbye!");
-       return userInt;
+        System.out.println(exPrompt);
+        return userInt;
     }
     public static void main (String[] args)
     {
-        getValue("Welcome! Enter a number: ");
+        getValue("Welcome! Enter a number: ", -10, 10, "Your number is not within my chosen bounds. Enter again: ", "Goodbye!");
     }
 }
+
